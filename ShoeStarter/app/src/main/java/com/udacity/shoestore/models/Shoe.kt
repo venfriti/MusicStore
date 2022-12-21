748ace5808eb4870ec418482122d599c339868aa
Tolu <tolusanusi6@gmail.com>
2022-12-21 07:53:53 +0100

    Delete ShoeStarter directory

--- ShoeStarter/app/src/main/java/com/udacity/shoestore/models/Shoe.kt
+++
@@ -1,8 +0,0 @@
-package com.udacity.shoestore.models
-
-
-data class Shoe(var name: String="",
-                var size: String="",
-                var company: String="",
-                var description: String="",
-                val images: List<String> = mutableListOf())
