1d64c09fca25d52f4361d96bb151d6e0b7701641
venfriti <tolusanusi6@gmail.com>
2023-09-28 10:07:45 +0100

    fix theme error in dark mode

--- app/src/main/java/com/example/musicstore/WelcomeFragment.kt
+++ app/src/main/java/com/example/musicstore/WelcomeFragment.kt
@@ -16,7 +16,7 @@ class WelcomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_welcome, container, false)
